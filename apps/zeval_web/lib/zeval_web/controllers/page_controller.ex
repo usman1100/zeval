c0521d41809c163c8b3e275184e40178fcd0ba97
Usman Ahmed <42866718+usman1100@users.noreply.github.com>
2026-06-14 11:21:24 +0500

    redirect authenticated users from / to /dashboard

--- apps/zeval_web/lib/zeval_web/controllers/page_controller.ex
+++ apps/zeval_web/lib/zeval_web/controllers/page_controller.ex
@@ -2,8 +2,12 @@ defmodule ZevalWeb.PageController do
   use ZevalWeb, :controller
 
   def index(conn, _params) do
-    conn
-    |> put_resp_header("content-type", "text/html; charset=utf-8")
-    |> send_resp(200, Phoenix.View.render_to_string(ZevalWeb.PageView, "index.html", %{}))
+    if get_session(conn, :current_user_id) do
+      redirect(conn, to: "/dashboard")
+    else
+      conn
+      |> put_resp_header("content-type", "text/html; charset=utf-8")
+      |> send_resp(200, Phoenix.View.render_to_string(ZevalWeb.PageView, "index.html", %{}))
+    end
   end
 end
